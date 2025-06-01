52c216659b2bebe365087f2d9fbd5e7752f4603e
adriatls <adria.tavares28@gmail.com>
2025-05-31 21:17:13 -0300

    feat: adicionado tooltip para o campo cnes e comportamento de habilitar o campo a depender do checkbox de agente de saúde

--- src/pages/cadastro/sendRegister.js
+++ src/pages/cadastro/sendRegister.js
@@ -1,33 +1,62 @@
-import RegisterManager from '../../services/registerService/registerManager.mjs';
+import RegisterManager from "../../services/registerService/registerManager.mjs";
 
-const form = document.getElementById('registrationForm');
+const checkboxHealthAgent = document.getElementById("checkbox-health-agent");
+const cnesInput = document.getElementById("cnes");
 
-form.addEventListener('submit', async function(event) {
-  event.preventDefault(); 
-  const sendRegister              = new RegisterManager();
-  const userName                  = document.getElementById('userName').value;
-  const userNascentDate           = document.getElementById('userNascentDate').value;
-  const userGender                = document.getElementById('userGender').value || 'Prefiro não informar';
-  const userEmail                 = document.getElementById('userEmail').value;
-  const userConfirmationEmail     = document.getElementById('userConfirmationEmail').value;
-  const userPassword              = document.getElementById('userPassword').value;
-  const userConfirmationPassword  = document.getElementById('userConfirmationPassword').value;
+function toggleCnesInput() {
+  if (checkboxHealthAgent.checked) {
+    cnesInput.disabled = false;
+    cnesInput.setAttribute("required", "required");
+    cnesInput.setAttribute("title", "Até 8 dígitos")
+  } else {
+    cnesInput.disabled = true;
+    cnesInput.removeAttribute("required");
+    cnesInput.setAttribute("title", "Selecione a opção 'Sou Agente de Saúde' para preencher este campo");
+    cnesInput.value = "";
+  }
+}
+
+// Executa ao carregar a página e ao alterar o checkbox
+checkboxHealthAgent.addEventListener("change", toggleCnesInput);
+window.addEventListener("DOMContentLoaded", toggleCnesInput);
+cnesInput.addEventListener("input", () => {
+  cnesInput.value = cnesInput.value.replace(/\D/g, "").slice(0, 8);
+});
+
+const form = document.getElementById("registrationForm");
+
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const sendRegister = new RegisterManager();
+  const userName = document.getElementById("userName").value;
+  const userNascentDate = document.getElementById("userNascentDate").value;
+  const userGender =
+    document.getElementById("userGender").value || "Prefiro não informar";
+  const userEmail = document.getElementById("userEmail").value;
+  const userConfirmationEmail = document.getElementById(
+    "userConfirmationEmail"
+  ).value;
+  const userPassword = document.getElementById("userPassword").value;
+  const userConfirmationPassword = document.getElementById(
+    "userConfirmationPassword"
+  ).value;
 
   try {
-    const registerData = await sendRegister.makeRegister({ 
-      userName:                   userName, 
-      userNascentDate:            userNascentDate, 
-      userGender:                 userGender, 
-      userEmail:                  userEmail, 
-      userConfirmationEmail:      userConfirmationEmail, 
-      userPassword:               userPassword,
-      userConfirmationPassword:   userConfirmationPassword
-    }); 
-    window.location.href = '../login/login.html';
-    alert('Cadastro realizado com sucesso!\nVocê já pode fazer login');
+    const registerData = await sendRegister.makeRegister({
+      userName: userName,
+      userNascentDate: userNascentDate,
+      userGender: userGender,
+      userEmail: userEmail,
+      userConfirmationEmail: userConfirmationEmail,
+      userPassword: userPassword,
+      userConfirmationPassword: userConfirmationPassword,
+    });
+    window.location.href = "../login/login.html";
+    alert("Cadastro realizado com sucesso!\nVocê já pode fazer login");
   } catch (error) {
     console.log(error);
-    alert('Ocorreu um erro ao realizar o cadastro. Por favor, tente novamente.');
+    alert(
+      "Ocorreu um erro ao realizar o cadastro. Por favor, tente novamente."
+    );
   }
-}) 
-
+});
